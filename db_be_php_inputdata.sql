4136d9e84cf255b3f671dfcfa5c2777d390e006e
David Soerdjana <davidsoerdjana@gmail.com>
2020-05-29 18:50:49 +0700

    Update and rename db_backend_php_inputdata.sql to db_be_php_inputdata.sql

--- db_be_php_inputdata.sql
+++ db_be_php_inputdata.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `db_backend_php_inputdata`
+-- Database: `db_be_php_inputdata`
 --
 
 -- --------------------------------------------------------
